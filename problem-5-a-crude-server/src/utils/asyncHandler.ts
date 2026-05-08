b06f492d570c7c3ed5f2ac63b24d94e45c78cb6f
sy-truong-nef <sy.truong.nef@gmail.com>
2026-05-08 11:10:24 +0300

    CHORE add async handler for routes

---
+++ problem-5-a-crude-server/src/utils/asyncHandler.ts
@@ -0,0 +1,9 @@
+import type { Request, Response, NextFunction, RequestHandler } from 'express';
+
+export const asyncHandler =
+  <Req extends Request = Request, Res extends Response = Response>(
+    fn: (req: Req, res: Res, next: NextFunction) => Promise<unknown>,
+  ): RequestHandler =>
+  (req, res, next) => {
+    Promise.resolve(fn(req as Req, res as Res, next)).catch(next);
+  };
